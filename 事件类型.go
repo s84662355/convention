b3ad60177b261c1eb12649eda2aa58555ad8b15f
陈嘉豪 <271375247@qq.com>
2021-08-12 16:38:57 +0800

    a

--- 事件类型.go
+++ 事件类型.go
@@ -2,6 +2,6 @@ package convention
 
 //事件类型 从1递增
 const (
-    EventConnectSucceed = iota + 1 //客户端 连接成功事件
-
+	EventConnectSucceed = iota + 1 //客户端 连接成功事件
+	EventLogin                     ////服务器端 登陆事件
 )
